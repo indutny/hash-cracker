98a28b9396f65e0479214aff3fcef339c0d16c54
Fedor Indutny <fedor@indutny.com>
2017-02-28 02:20:09 -0500

    optimize bandwidth

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const http = require('http');
+const zlib = require('zlib');
 
 http.createServer((req, res) => {
   let chunks = '';
-  req.on('data', chunk => chunks += chunk);
+  req.on('data', chunk => chunks += chunk;
   req.once('end', () => {
     let body;
     try {
-      body = JSON.parse(chunks);
+      const buf = Buffer.from(chunks, 'base64');
+      body = JSON.parse(zlib.inflateSync(buf).toString());
     } catch (e) {
     }
     res.end();
